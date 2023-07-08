c21a42c5f10a59594cb2a6e8dcc6f8774cd4d40c
Leo Sobecki <leosobecki@gmail.com>
2023-07-08 12:47:24 +0200

    Added index.js and updated infocomponent

--- Amino-app/amino-app/src/components/InfoComponent.js
+++ Amino-app/amino-app/src/components/InfoComponent.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import AminoAcid from './AminoAcid';
+import React, { useState } from "react";
+import AminoAcid from "./AminoAcid";
 
-// Functional component: info panel for displaying information about amino acids.
-const InfoComponent = ({ aminoAcidData }) => {
-    // The useState hook manages the state of the currently selected amino acid.
-    const [selectedAminoAcid, setSelectedAminoAcid] = useState(aminoAcidData[0]);  // select first amino acid by default
+const InfoComponent = ({ switchComponent, aminoAcidData }) => {
+  const [selectedAminoAcid, setSelectedAminoAcid] = useState(aminoAcidData[0]); // select first amino acid by default
 
-    // handleInput handles input from the user and uses it to set the selected amino acid.
-    const handleInput = (input) => {
-        // logic to select amino acid based on input
-        setSelectedAminoAcid(input);
-    };
+  const handleInput = (input) => {
+    // logic to select amino acid based on input
+    setSelectedAminoAcid(input);
+  };
 
-    // InfoComponent renders an AminoAcid component with the currently selected amino acid.
-    return (
-        <div>
-            <AminoAcid {...selectedAminoAcid} onInput={handleInput} />
-        </div>
-    );
+  // InfoComponent renders an AminoAcid component with the currently selected amino acid.
+  return (
+    <div>
+      <AminoAcid {...selectedAminoAcid} onInput={handleInput} />
+      {/* Button to switch to another component */}
+      <button onClick={() => switchComponent("OtherComponent")}>
+        Switch to Other Component
+      </button>
+    </div>
+  );
 };
 
 export default InfoComponent;
